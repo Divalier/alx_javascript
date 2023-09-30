f9e0da588d680fe0f105c0602f666c7a5de73ea6
Divalier <111007588+Divalier@users.noreply.github.com>
2023-09-30 12:48:05 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
+      if (!w || !h || w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
           // If w or h is equal to 0 or not a positive integer, create an empty object
           return {};
       }
