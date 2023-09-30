923c0ba81737bdbbcc617bf3aeb8b6adcb18bac4
Divalier <111007588+Divalier@users.noreply.github.com>
2023-09-30 12:14:03 +0200

    class

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size); // Call the constructor of the parent class (Rectangle) with size for both width and height
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Square;
+  
