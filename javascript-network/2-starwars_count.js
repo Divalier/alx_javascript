ad60dc58020618b38b1bc513f40f4e195c4a2b7b
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-03 01:13:03 +0200

    Create 2-starwars_count.js

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,18 @@
+const req = require("request");
+let char = [];
+const id = 18;
+const url = `https://swapi-api.alx-tools.com/api/films`;
+req.get(url, (error, response, body) => {
+  const movies = JSON.parse(body);
+  const moviesres = movies.results;
+  moviesres.forEach((element) => {
+    // ch.push(element.characters)
+    // console.log(ch);
+    element.characters.forEach((el) => {
+      if (el.includes(id)) {
+        char.push(el);
+      }
+    });
+  });
+  return char.length;
+});
